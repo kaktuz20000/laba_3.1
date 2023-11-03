36b754be984927430b9cb19ad496c3fb2c61a287
kaktuz20000 <149298757+kaktuz20000@users.noreply.github.com>
2023-11-03 11:37:17 +0300

    laba_3

---
+++ js/main.js
@@ -0,0 +1,20 @@
+setInterval(() => {
+    const time = document.querySelector(".display #time");
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    if (hours > 23) {
+        hours = 0;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    time.textContent = hours + ":" + minutes + ":" + seconds; 
+})
